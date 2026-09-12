feb4eeec57e574cf26b4a8379a09f6ba0b764a4b
Toby Huang <tobyhuang@Tobys-MBP.attlocal.net>
2026-09-12 16:11:03 -0700

    Convert rope.py to vectorized operations and avoid dense matrices

--- cs336_basics/rope.py
+++ cs336_basics/rope.py
@@ -1,29 +1,32 @@
 import torch.nn as nn
 import torch
-from math import cos, sin
-from einops import einsum
+from einops import rearrange
 
 class RotaryPositionalEmbedding(nn.Module):
     def __init__(self, theta: float, d_k: int, max_seq_len: int, device=None):
         super().__init__()
-        thetas = [[i / theta**((2*k-2)/d_k) for k in range(1, d_k//2 + 1)] for i in range(max_seq_len)]
-        self.register_buffer("R", torch.zeros(max_seq_len, d_k, d_k, device=device), persistent=False)
-        for i in range(max_seq_len):
-            for k in range(d_k//2):
-                self.R[i][2*k][2*k] = cos(thetas[i][k])
-                self.R[i][2*k][2*k+1] = -sin(thetas[i][k])
-                self.R[i][2*k+1][2*k] = sin(thetas[i][k])
-                self.R[i][2*k+1][2*k+1] = cos(thetas[i][k])
+        i = torch.arange(0, max_seq_len)
+        i = rearrange(i, "... -> ... 1")
+        k = torch.arange(1, d_k//2+1)
+        thetas = i / theta**((2*k-2)/d_k)
+        cos = torch.cos(thetas)
+        sin = torch.sin(thetas)
+        self.register_buffer("cos", cos, persistent=False)
+        self.register_buffer("sin", sin, persistent=False)
         
     def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
-        rotations = self.R[token_positions]
-        return einsum(rotations, x, "seq_len col_out row_in, ... seq_len row_in -> ... seq_len col_out")
+        cos = self.cos[token_positions]
+        sin = self.sin[token_positions]
+        evens = x[..., 0::2]
+        odds = x[..., 1::2]
+        result = torch.stack([evens * cos - odds * sin, evens * sin + odds * cos], dim=-1)
+        return rearrange(result, "... pair two -> ... (pair two)")
         
 
 if __name__ == '__main__':
     theta = 100
-    d_k = 64
-    max_seq_len = 12
+    d_k = 128
+    max_seq_len = 2048
     embedding = RotaryPositionalEmbedding(theta, d_k, max_seq_len)
     in_query_or_key = torch.ones(max_seq_len, d_k)
     token_positions = torch.arange(0, max_seq_len)
